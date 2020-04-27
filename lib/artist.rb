ab952cac73e7ba9189edf78f62138e1a92bbbc58
Devone Austin <devone.austin@gmail.com>
2020-04-27 23:48:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,21 @@
 
-
 class Artist
-
   attr_accessor :name
-
-  def initialize (name)
+  def initialize(name)
     @name = name
     @songs = []
   end
-
-  def songs()
-
+  def songs
+    Song.all.select{|song| song.artist == self}
   end
-
   def add_song(song)
-
+    song.artist = self
   end
-
-  def add_song_by_name(song)
-
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
   end
-
-  def self.song_count(song)
-
+  def self.song_count
+    Song.all.length
   end
-
 end
